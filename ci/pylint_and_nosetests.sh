8e0b9c9c733c10d023713393842fb3acc005a712
Aymen <aymendjellal@gmail.com>
2020-12-02 02:22:15 -0500

    Update pylint_and_nosetests.sh

--- ci/pylint_and_nosetests.sh
+++ ci/pylint_and_nosetests.sh
@@ -10,12 +10,12 @@ echo EXIT_CODE=$EXIT_CODE
 pylint $PYLINTOPTS --jobs=0 $PYLINTFILES  || EXIT_CODE=$(($EXIT_CODE || $?))
 echo EXIT_CODE PYLINTOPTS=$EXIT_CODE
 
+
 echo
 echo "========================================="
 echo "nosetests output:"
 echo "========================================="
-nosetests tests $NOSEOPTS  || EXIT_CODE=$(($EXIT_CODE || $?))
+sudo env "PATH=$PATH" nosetests tests $NOSEOPTS  || EXIT_CODE=$(($EXIT_CODE || $?))
 echo EXIT_CODE nosetests=$EXIT_CODE
-
 
 exit "$EXIT_CODE"
