f5572c99a14abeacf7ea905c02df9d81edd06ec9
Aymen <aymendjellal@gmail.com>
2020-12-02 02:09:25 -0500

    Update pylint_and_nosetests.sh

--- ci/pylint_and_nosetests.sh
+++ ci/pylint_and_nosetests.sh
@@ -6,18 +6,22 @@ EXIT_CODE=0
 echo "========================================="
 echo "pylint output:"
 echo "========================================="
+echo EXIT_CODE=$EXIT_CODE
 pylint $PYLINTOPTS --jobs=0 $PYLINTFILES  || EXIT_CODE=$(($EXIT_CODE || $?))
+echo EXIT_CODE PYLINTOPTS=$EXIT_CODE
 
 echo
 echo "========================================="
 echo "nosetests -a '!requires_sudo' output:"
 echo "========================================="
 nosetests -a '!requires_sudo' tests $NOSEOPTS  || EXIT_CODE=$(($EXIT_CODE || $?))
+echo EXIT_CODE nosetests=$EXIT_CODE
 
 echo
 echo "========================================="
 echo "nosetests -a 'requires_sudo' output:"
 echo "========================================="
 sudo env "PATH=$PATH" nosetests -a 'requires_sudo' tests $NOSEOPTS  || EXIT_CODE=$(($EXIT_CODE || $?))
+echo EXIT_CODE sudonosetests=$EXIT_CODE
 
 exit "$EXIT_CODE"
