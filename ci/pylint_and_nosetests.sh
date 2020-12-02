30c170abb20669f25b92ab8e859438e0f25ddec4
Aymen <aymendjellal@gmail.com>
2020-12-02 17:49:37 -0500

    Update pylint_and_nosetests.sh

--- ci/pylint_and_nosetests.sh
+++ ci/pylint_and_nosetests.sh
@@ -1,21 +1,30 @@
 #!/usr/bin/env bash
 
 set -u
+
+pylint $PYLINTOPTS --jobs=0 $PYLINTFILES &> pylint.output & PYLINT_PID=$!
+
 EXIT_CODE=0
+
+echo "========================================="
+echo "nosetests -a '!requires_sudo' output"
+echo "========================================="
+nosetests -a '!requires_sudo' tests $NOSEOPTS || EXIT_CODE=$(($EXIT_CODE || $?))
+echo EXIT_CODE nosetests = $?
+
+
+echo "========================================="
+echo "nosetests -a 'requires_sudo' output"
+echo "========================================="
+sudo env "PATH=$PATH" nosetests -a 'requires_sudo' tests $NOSEOPTS || EXIT_CODE=$(($EXIT_CODE || $?))
+echo EXIT_CODE sudo nosetest = $?
 
 echo "========================================="
 echo "pylint output:"
 echo "========================================="
-echo EXIT_CODE=$EXIT_CODE
+wait $PYLINT_PID || EXIT_CODE=$(($EXIT_CODE || $?))
 pylint $PYLINTOPTS --jobs=0 $PYLINTFILES  || EXIT_CODE=$(($EXIT_CODE || $?))
-echo EXIT_CODE PYLINTOPTS=$EXIT_CODE
+echo EXIT_CODE pylint =  $?
 
-
-echo
-echo "========================================="
-echo "nosetests output:"
-echo "========================================="
-sudo env "PATH=$PATH" nosetests tests $NOSEOPTS  || EXIT_CODE=$(($EXIT_CODE || $?))
-echo EXIT_CODE nosetests=$EXIT_CODE
-
+echo Final EXIT_CODE = $EXIT_CODE
 exit "$EXIT_CODE"
