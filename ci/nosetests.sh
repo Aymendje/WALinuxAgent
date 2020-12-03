cbcb5d306224f6456f45dd8e0c46980105066a5b
Aymen <aymendjellal@gmail.com>
2020-12-03 01:27:31 -0500

    Moving pylint to pipeline

--- ci/nosetests.sh
+++ ci/nosetests.sh
@@ -2,31 +2,21 @@
 
 set -u
 
-pylint $PYLINTOPTS --jobs=0 $PYLINTFILES &> pylint.output & PYLINT_PID=$!
-
 EXIT_CODE=0
 
 echo "========================================="
 echo "nosetests -a '!requires_sudo' output"
 echo "========================================="
-nosetests -a '!requires_sudo' tests $NOSEOPTS || EXIT_CODE=$(($EXIT_CODE || $?))
+nosetests -a '!requires_sudo' tests $(echo "${NOSEOPTS/__uuid__/$(uuidgen)}") || EXIT_CODE=$(($EXIT_CODE || $?))
 echo EXIT_CODE nosetests = $EXIT_CODE
 sudo mv .coverage coverage.$(uuidgen).no_requires_sudo.data
 
 echo "========================================="
 echo "nosetests -a 'requires_sudo' output"
 echo "========================================="
-sudo env "PATH=$PATH" nosetests -a 'requires_sudo' tests $NOSEOPTS || EXIT_CODE=$(($EXIT_CODE || $?))
+sudo env "PATH=$PATH" nosetests -a 'requires_sudo' tests $(echo "${NOSEOPTS/__uuid__/$(uuidgen)}") || EXIT_CODE=$(($EXIT_CODE || $?))
 echo EXIT_CODE sudo nosetest = $EXIT_CODE
 sudo mv .coverage coverage.$(uuidgen).requires_sudo.data
-
-wait $PYLINT_PID || EXIT_CODE=$(($EXIT_CODE || $?))
-echo "========================================="
-echo "pylint output:"
-echo "========================================="
-cat pylint.output
-rm pylint.output
-echo EXIT_CODE pylint = $EXIT_CODE
 
 echo Final EXIT_CODE = $EXIT_CODE
 exit "$EXIT_CODE"
