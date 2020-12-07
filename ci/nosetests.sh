1c91a7572f5976f1da6f5e700f559b071c15fc86
Aymen Djellal <aymendjellal@gmail.com>
2020-12-07 16:40:16 -0500

    Update nosetests.sh

--- ci/nosetests.sh
+++ ci/nosetests.sh
@@ -7,7 +7,7 @@ EXIT_CODE=0
 echo "========================================="
 echo "nosetests -a '!requires_sudo' output"
 echo "========================================="
-nosetests -a '!requires_sudo' tests $(echo "${NOSEOPTS/__uuid__/$(uuidgen)}") || EXIT_CODE=$(($EXIT_CODE || $?))
+sudo setpriv --reuid 65534 --regid 65534 --clear-groups nosetests -a '!requires_sudo' tests $(echo "${NOSEOPTS/__uuid__/$(uuidgen)}") || EXIT_CODE=$(($EXIT_CODE || $?))
 echo EXIT_CODE nosetests = $EXIT_CODE
 mv .coverage coverage.$(uuidgen).no_requires_sudo.data
 
